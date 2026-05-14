65cbd211d521e582c2b90074a54b8ee8a26cbf5f
sckwoky <sckwokyboom@gmail.com>
2026-05-14 18:21:58 +0700

    build: add JavaParser core + JSON schema validator (test scope)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,11 @@ dependencies {
     implementation("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
     implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("com.github.javaparser:javaparser-core:3.27.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
     testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("com.networknt:json-schema-validator:1.5.5")
 }
 
 application {
